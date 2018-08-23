14e7d1eae71ba78643de5fa4083a625789011a15
Steerlean <38030193+ShraddhaAgarwal@users.noreply.github.com>
2018-08-23 14:45:41 +0530

    Update StudentController.java

--- src/main/java/com/steerlean/controller/StudentController.java
+++ src/main/java/com/steerlean/controller/StudentController.java
@@ -4,9 +4,9 @@ import com.steerlean.model.Course;
 import com.steerlean.model.Student;
 import com.steerlean.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+// import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
